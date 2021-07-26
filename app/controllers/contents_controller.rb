0d7d227b9f5b3984436c9830c6a5ff99199a70ca
John Diego <johnoliveirasilva9@gmail.com>
2021-07-25 22:44:57 -0300

    Adicionando form de cadastro de contéudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,6 +3,29 @@ class ContentsController < ApplicationController
 
   def index
     @contents = current_user.contents
-    
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Contente.new(content_params)
+
+    if @content.save
+      redirect_to @content
+    else
+      render new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :description)
+  end
+
+  def set_content
+    @content = Content.find(params[:id])
   end
 end
