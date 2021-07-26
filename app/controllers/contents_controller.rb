0c503a6467e4b36ceb0a0d49abd25d67544ec660
John Diego <johnoliveirasilva9@gmail.com>
2021-07-25 23:11:37 -0300

    Adiciona tela de edição de Conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,6 @@
 class ContentsController < ApplicationController
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
+  before_action :set_content, only: [:edit, :show, :update]
 
   def index
     @contents = current_user.contents
@@ -10,15 +11,25 @@ class ContentsController < ApplicationController
   end
 
   def create
-    @content = Contente.new(content_params)
+    @content = current_user.contents.build(content_params)
 
     if @content.save
-      redirect_to @content
+      redirect_to @content, notice: 'Content successfully created!'
     else
-      render new
+      render :new
     end
   end
+  def edit; end
 
+  def show; end
+
+  def update
+    if @content.update(content_params)
+      redirect_to @content, notice: 'Content successfully updated!'
+    else
+      render :edit
+    end
+  end
   private
 
   def content_params
